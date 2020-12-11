df59f8177fada6c3dace8a5eb0ba85c889d53197
flairmix <d9067246810@gmail.com>
2020-12-11 14:06:55 +0300

    0 init

---
+++ hr_0/0_Variable Sized Arrays.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    // number of arrays elements 
+    int n;
+    // number of queries 
+    int q;
+
+    cin >> n >> q;
+    vector<vector<int>> a (n);
+
+    for (auto index=0;  index < n; ++index){
+        int n_insert; 
+        // vector<int> temp;
+        cin >> n_insert;
+        for (auto i=0; i < n_insert; ++i){
+            int temp;
+            cin >> temp;
+            a.at(index).push_back(temp);
+        }
+    }
+    // a.insert(0,);
+
+    cout << a.size() << endl;
+    // cout<< a.at(0) << endl;
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
